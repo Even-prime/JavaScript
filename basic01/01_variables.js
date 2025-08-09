6f1ee7414edbd38ee4cf56e15bd7500e6ec8fa08
Md Abulich Lupon <167802435+Even-prime@users.noreply.github.com>
2025-08-09 17:11:16 +0000

    class on variables

---
+++ basic01/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 2004042
+let accountEmail = 'sopnil.lupon@gmail.com'
+var accountPassword = '12345lupon'
+accountCity = 'Kurigram'
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity])
