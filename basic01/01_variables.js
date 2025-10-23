eab4fa22f28dd22198cd432c06cff652951a3812
Md Abulich Lupon <167802435+Even-prime@users.noreply.github.com>
2025-10-23 19:19:07 +0000

    testing the blue points

--- basic01/01_variables.js
+++ basic01/01_variables.js
@@ -2,7 +2,8 @@ const accountId = 2004042
 let accountEmail = 'sopnil.lupon@gmail.com'
 var accountPassword = '12345lupon'
 accountCity = 'Kurigram'
+let acountHolder;
 
 console.log(accountId);
 
-console.table([accountEmail,accountPassword,accountCity])
+console.table([accountEmail,accountPassword,accountCity,acountHolder])
